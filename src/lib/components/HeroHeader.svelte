ac8e40897c52edfbc0a594c47c09d8b478e5e0da
sipofnightshade <gregorymcmillan96@gmail.com>
2024-10-08 19:45:41 -0400

    added-- HeroHeader component (incomplete)

---
+++ src/lib/components/HeroHeader.svelte
@@ -0,0 +1,7 @@
+<h1 class="w-full flex-col pb-16 text-[16rem] leading-none">
+  <strong class="relative -left-2">developer</strong>
+  <strong class="flex w-full justify-end gap-6">
+    <span class="text-gray-300">and</span>
+    <span class="text-brand">designer</span>
+  </strong>
+</h1>
