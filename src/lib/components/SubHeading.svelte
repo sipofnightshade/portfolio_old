0174e91be146b7e81efb7b7cc698792ea6d55018
sipofnightshade <gregorymcmillan96@gmail.com>
2024-10-08 19:45:21 -0400

    added-- SubHeading component

---
+++ src/lib/components/SubHeading.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  let { title, desc, dark = true } = $props();
+</script>
+
+<div class="flex flex-col">
+  <h2
+    class="mt-10 text-9xl font-bold leading-tight"
+    class:text-gray-200={dark}
+    class:text-black={!dark}
+  >
+    {title}
+  </h2>
+  <div class="flex gap-8">
+    <div
+      class="mt-2 h-1.5 w-14 bg-white"
+      class:bg-white={dark}
+      class:bg-black={!dark}
+    ></div>
+    <p
+      class="max-w-80 font-mono text-lg font-bold leading-snug"
+      class:text-gray-400={dark}
+      class:text-black={!dark}
+    >
+      {desc}
+    </p>
+  </div>
+</div>
