69467508a804aa3daa80d894f4f88c46dfb35a0a
sipofnightshade <gregorymcmillan96@gmail.com>
2024-10-08 21:29:54 -0400

    added-- link icon

---
+++ src/lib/icons/Link.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  let { classes = '' } = $props();
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" class={classes} viewBox="0 0 45 44" fill="none"
+  ><path
+    stroke="#fff"
+    stroke-linecap="square"
+    stroke-width="8"
+    d="M14.864 4H40.5v25.636M6 38 38 6"
+  />
+</svg>
