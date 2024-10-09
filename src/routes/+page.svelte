cb852006c119af2aac6c4d7734c93132781c2251
sipofnightshade <gregorymcmillan96@gmail.com>
2024-10-08 20:18:46 -0400

    added-- about section finished (mostly)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,3 +12,21 @@
   <Navbar />
   <HeroHeader />
 </section>
+
+<section class="h-svh bg-black">
+  <div class="container mx-auto grid grid-cols-2 py-40">
+    <SubHeading title="about." desc="Just some short notes about myself and my goals." />
+    <div class="text-4xl font-bold leading-snug text-gray-200">
+      <p>
+        I am a Frontend Developer and UI/UX Designer from the Caribbean—one of the many
+        small islands. I specialize in crafting intuitive interfaces that bridge the gap
+        between technology and everyday life.
+      </p>
+      <br />
+      <p>
+        My aim is always to design solutions that seamlessly integrate into people's
+        lives, regardless of their technical background.
+      </p>
+    </div>
+  </div>
+</section>
