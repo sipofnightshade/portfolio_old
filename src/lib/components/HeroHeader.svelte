39b677a9b71949fcd11dad8a52e5564e9c1e4b8a
sipofnightshade <gregorymcmillan96@gmail.com>
2024-10-10 13:52:50 -0400

    fixed-- HeroHeader so that vertical lines are correctly aligned with text

--- src/lib/components/HeroHeader.svelte
+++ src/lib/components/HeroHeader.svelte
@@ -1,7 +1,7 @@
 <h1 class="w-full flex-col pb-16 text-[16rem] leading-none">
-  <strong class="relative -left-2">developer</strong>
-  <strong class="flex w-full justify-end gap-6">
-    <span class="text-gray-300">and</span>
-    <span class="text-brand">designer</span>
+  <strong class="relative -left-4 font-mono font-bold tracking-tighter">developer</strong>
+  <strong class="relative -right-1 flex w-full justify-between gap-6 font-semibold">
+    <span class="relative -left-3.5 text-gray-300">and</span>
+    <span class="relative text-brand">designer</span>
   </strong>
 </h1>
