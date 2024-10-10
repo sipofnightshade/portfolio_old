d759d277406fbe36741804bc0e388c9907a31f2c
sipofnightshade <gregorymcmillan96@gmail.com>
2024-10-09 20:56:02 -0400

    updated-- BackgroundLines to be reusable for dark backgrounds

--- src/lib/components/BackgroundLines.svelte
+++ src/lib/components/BackgroundLines.svelte
@@ -1,15 +1,34 @@
 <script lang="ts">
+  let { dark = false } = $props();
 </script>
 
-<div
-  class="bg_lines absolute left-0 top-0 -z-10 h-full w-full border-x border-gray-200"
-></div>
+{#if dark}
+  <div
+    class="bg_lines_dark absolute left-0 top-0 -z-10 h-full w-full border-x border-zinc-900"
+  ></div>
+{:else}
+  <div
+    class="bg_lines_light absolute left-0 top-0 -z-10 h-full w-full border-x border-gray-200"
+  ></div>
+{/if}
 
 <style lang="postcss">
-  .bg_lines {
+  .bg_lines_light {
     --w: 1px; /* Thiclness of the line */
     --n: 3; /* number of lines */
     --c: theme(colors.gray.200);
+
+    background: linear-gradient(90deg, #0000 calc(100% - var(--w)), var(--c) 0);
+    background-size: calc(
+        (100% - var(--n) * var(--w)) / (var(--n) + 1) + var(--w) + 2 * var(--w)
+      )
+      100%;
+  }
+
+  .bg_lines_dark {
+    --w: 1px; /* Thiclness of the line */
+    --n: 3; /* number of lines */
+    --c: theme(colors.zinc.900);
 
     background: linear-gradient(90deg, #0000 calc(100% - var(--w)), var(--c) 0);
     background-size: calc(
