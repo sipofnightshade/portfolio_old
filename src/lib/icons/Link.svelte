5fc2a2973184ea22df5d71fd9318e9776e0811f1
sipofnightshade <gregorymcmillan96@gmail.com>
2024-10-10 22:09:31 -0400

    fixed-- text-color not being applied correctly to Link

--- src/lib/icons/Link.svelte
+++ src/lib/icons/Link.svelte
@@ -4,7 +4,7 @@
 
 <svg xmlns="http://www.w3.org/2000/svg" class={classes} viewBox="0 0 45 44" fill="none"
   ><path
-    stroke="#fff"
+    stroke="currentColor"
     stroke-linecap="square"
     stroke-width="8"
     d="M14.864 4H40.5v25.636M6 38 38 6"
