e4589f0470edbc41c97480e83794f7caa91d2251
sipofnightshade <gregorymcmillan96@gmail.com>
2024-10-11 23:12:52 -0400

    finished-- header stagger animation

--- src/lib/components/HeroHeader.svelte
+++ src/lib/components/HeroHeader.svelte
@@ -1,7 +1,29 @@
-<h1 class="w-full flex-col pb-16 text-[16rem] leading-none">
-  <strong class="relative -left-4 font-mono font-bold tracking-tighter">developer</strong>
-  <strong class="relative -right-1 flex w-full justify-between gap-6 font-semibold">
-    <span class="relative -left-3.5 text-gray-300">and</span>
-    <span class="relative text-brand">designer</span>
+<script lang="ts">
+  import { gsap } from 'gsap';
+
+  $effect(() => {
+    gsap.to('.testing', {
+      y: 0,
+      duration: 1,
+      stagger: 0.2
+    });
+  });
+</script>
+
+<h1 class=" w-full flex-col pb-16 text-[16rem] leading-none">
+  <strong
+    class="relative -left-1 flex w-full justify-between gap-6 font-semibold tracking-tighter"
+  >
+    <span class="testing relative -left-3.5 translate-y-80 font-mono">developer</span>
+  </strong>
+  <strong class="relative -right-1 flex w-full justify-between gap-6 pb-5 font-semibold">
+    <span class="testing relative -left-3.5 translate-y-80 text-gray-300">and</span>
+    <span class="testing relative translate-y-80 text-brand">designer</span>
   </strong>
 </h1>
+
+<style>
+  strong {
+    clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
+  }
+</style>
