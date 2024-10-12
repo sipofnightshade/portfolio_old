1f5943f7da21f57c6c435fc287f763a7ca5acfb0
sipofnightshade <gregorymcmillan96@gmail.com>
2024-10-11 23:20:54 -0400

    fixed-- added "pl-1" to fix small cut off from "and"

--- src/lib/components/HeroHeader.svelte
+++ src/lib/components/HeroHeader.svelte
@@ -10,14 +10,14 @@
   });
 </script>
 
-<h1 class=" mb-12 w-full flex-col text-[16rem] leading-none">
+<h1 class="mb-12 w-full flex-col text-[16rem] leading-none">
   <strong
     class="relative -left-1 flex w-full justify-between gap-6 font-semibold tracking-tighter"
   >
     <span class="testing relative -left-3.5 translate-y-80 font-mono">developer</span>
   </strong>
   <strong class="relative -right-1 flex w-full justify-between gap-6 pb-5 font-semibold">
-    <span class="testing relative -left-3.5 translate-y-80 text-gray-300">and</span>
+    <span class="testing relative -left-3.5 translate-y-80 pl-1 text-gray-300">and</span>
     <span class="testing relative translate-y-80 text-brand">designer</span>
   </strong>
 </h1>
