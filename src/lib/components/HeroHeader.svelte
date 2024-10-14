e1a3eb07502195fd070a3580f59c240cece21b38
sipofnightshade <gregorymcmillan96@gmail.com>
2024-10-14 16:16:38 -0400

    adjusted-- font sizes and tracking for HeroHeader text

--- src/lib/components/HeroHeader.svelte
+++ src/lib/components/HeroHeader.svelte
@@ -10,14 +10,16 @@
   });
 </script>
 
-<h1 class="mb-12 w-full flex-col text-[16rem] leading-none">
+<h1 class="mb-12 w-full flex-col leading-none">
   <strong
-    class="relative -left-1 flex w-full justify-between gap-6 font-semibold tracking-[-0.08em]"
+    class="relative -left-1 flex w-full justify-between gap-6 text-[15.5rem] font-semibold tracking-[-0.08em]"
   >
     <span class="testing relative -left-3.5 translate-y-80 font-mono">developer</span>
   </strong>
-  <strong class="relative -right-1 flex w-full justify-between gap-6 pb-5 font-semibold">
-    <span class="testing relative -left-3.5 translate-y-80 pl-1 text-gray-300">and</span>
+  <strong
+    class="relative -right-1 flex w-full justify-end gap-2 pb-5 text-[15rem] font-semibold"
+  >
+    <span class="testing translate-y-80 pl-1 text-gray-300">and</span>
     <span class="testing relative translate-y-80 text-brand">designer</span>
   </strong>
 </h1>
