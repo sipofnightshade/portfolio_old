a4320fb90964d1e09f2001b2690c3bfca4a51e7f
sipofnightshade <gregorymcmillan96@gmail.com>
2024-10-19 19:41:14 -0400

    update-- navbar to new design

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,4 +1,12 @@
 <div class="flex w-full justify-between py-5 text-xl">
-  <strong>gregorymcmillan</strong>
-  <nav>lets talk!</nav>
+  <a href="/" class="text-2xl font-bold">Greg.</a>
+  <nav class="flex items-center gap-9 text-lg font-bold capitalize">
+    <a href="/about">About</a>
+    <a href="/work">Projects</a>
+
+    <a
+      href="/mailto:info@gregorymcmillan.me"
+      class="border-2 border-black bg-brand px-4 py-2 leading-none">Let's Talk</a
+    >
+  </nav>
 </div>
