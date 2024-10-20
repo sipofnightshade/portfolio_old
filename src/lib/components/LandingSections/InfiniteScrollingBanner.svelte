cbfeb7f5e4c84161d679e67535e4a8ff252c7684
sipofnightshade <gregorymcmillan96@gmail.com>
2024-10-19 22:01:30 -0400

    added-- InfiniteScrollingBanner component outline

---
+++ src/lib/components/LandingSections/InfiniteScrollingBanner.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  const tools = [
+    'Svelte',
+    'Tailwind',
+    'Appwrite',
+    'GSAP',
+    'Figma',
+    'TypeScript',
+    'Pocketbase'
+  ];
+</script>
+
+<div
+  class="flex h-48 items-center justify-center gap-6 bg-black stroke-brand font-semibold uppercase text-brand"
+>
+  {#each tools as tool}
+    <span class="text-5xl">{tool}</span>
+  {/each}
+</div>
