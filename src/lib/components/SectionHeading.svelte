3ef769c7d5a9cd60fef9500d74ced47cc0a369b5
sipofnightshade <gregorymcmillan96@gmail.com>
2024-10-19 22:17:32 -0400

    fixed-- SectionHeading styles

---
+++ src/lib/components/SectionHeading.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  let { title, desc = '' } = $props();
+</script>
+
+<div class="flex flex-col gap-4 font-bold">
+  <h2 class="text-2xl uppercase tracking-wider">{title}.</h2>
+  {#if desc}
+    <div class="flex max-w-80 gap-8 border-l-4 border-black bg-brand py-2 pl-2">
+      <p class="leading-tight">{desc}</p>
+    </div>
+  {/if}
+</div>
