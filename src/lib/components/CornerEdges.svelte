72042e181a9615aabdd7da7be2d2308319b2f516
sipofnightshade <gregorymcmillan96@gmail.com>
2024-10-19 22:50:35 -0400

    updated-- CornerEdges props to be more reusable

--- src/lib/components/CornerEdges.svelte
+++ src/lib/components/CornerEdges.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   import Corner from '$lib/icons/Corner.svelte';
+  let { strokeWidth = 2, class: className = 'h-3' } = $props();
 </script>
 
-<Corner class="absolute right-0 top-0 z-10 h-3" strokeWidth={2} />
-<Corner class="absolute bottom-0 right-0 z-10 h-3 rotate-90" strokeWidth={2} />
-<Corner class="absolute bottom-0 left-0 z-10 h-3 rotate-180" strokeWidth={2} />
-<Corner class="absolute left-0 top-0 z-10 h-3 -rotate-90" strokeWidth={2} />
+<Corner class="absolute right-0 top-0 z-10 {className}" {strokeWidth} />
+<Corner class="absolute bottom-0 right-0 z-10 rotate-90 {className}" {strokeWidth} />
+<Corner class="absolute bottom-0 left-0 z-10 rotate-180 {className}" {strokeWidth} />
+<Corner class="absolute left-0 top-0 z-10 -rotate-90 {className}" {strokeWidth} />
