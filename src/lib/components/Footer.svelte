69c80205d1cbbf798e792efbcd0d39f9de91099c
sipofnightshade <gregorymcmillan96@gmail.com>
2024-10-20 21:20:13 -0400

    update-- Contact section to new design

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,4 @@
-<footer class="col-span-2 my-8 flex items-center justify-between p-0 text-neutral-400">
+<footer class="col-span-2 my-8 flex items-center justify-between p-0 font-medium">
   <a href="emailto:info@gregorymcmillan.me" class="text-xl">info@gregorymcmillan.me</a>
   <a href="https://gregorymcmillan.me/privacy" target="_blank" class="text-xl"
     >privacy policy</a
--- src/lib/components/LandingSections/Contact.svelte
+++ src/lib/components/LandingSections/Contact.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-  import ContactForm from '../ContactForm.svelte';
+  import { Triangle, Pentagon, Diamond } from '$lib/icons';
   import Footer from '../Footer.svelte';
-  import SubHeading from '../SubHeading.svelte';
 
   interface Props {
     class?: string;
@@ -9,11 +8,31 @@
   let { class: className }: Props = $props();
 </script>
 
-<section class="h-screen {className}">
-  <div class="container mx-auto flex h-full flex-col justify-between pb-0 pt-40">
-    <div class="grid h-full grid-cols-2 gap-2">
-      <SubHeading title="contact." desc="Let's get to work!" />
-      <ContactForm />
+<section class="h-[104vh] {className}">
+  <div class="container mx-auto flex h-full flex-col justify-between pb-0 pt-20">
+    <div class="flex h-full flex-col items-center justify-between">
+      <div class="flex w-full items-center justify-between">
+        <div>
+          <p>Lorem ipsum dolor sit amet consectetur.</p>
+        </div>
+
+        <div class="flex h-11 w-11 items-center justify-center bg-black">
+          <Triangle class="h-5 fill-none stroke-zinc-200" />
+        </div>
+      </div>
+      <a
+        href="mailto:info@gregorymcmillan.me"
+        class="border-4 border-black bg-brand p-10 text-9xl font-semibold"
+        >Get in touch!</a
+      >
+      <div class="flex w-full items-center justify-between">
+        <div class="flex h-11 w-11 items-center justify-center bg-black">
+          <Pentagon class="h-6 fill-none stroke-zinc-200" />
+        </div>
+        <div class="flex h-11 w-11 items-center justify-center bg-black">
+          <Diamond class="h-6 fill-none stroke-zinc-200" />
+        </div>
+      </div>
     </div>
     <Footer />
   </div>
