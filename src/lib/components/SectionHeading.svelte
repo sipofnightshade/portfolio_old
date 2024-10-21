ec0be06cd7676bb558cad58c75c975b259f59db7
sipofnightshade <gregorymcmillan96@gmail.com>
2024-10-21 14:25:35 -0400

    updated-- section heading conditional classes to work for Service section

--- src/lib/components/SectionHeading.svelte
+++ src/lib/components/SectionHeading.svelte
@@ -1,16 +1,31 @@
 <script lang="ts">
-  let { title, desc = '', class: className } = $props();
+  let { title, desc = '', dark = false } = $props();
 </script>
 
-<div class="flex flex-col gap-4 {className}">
-  <h2 class="text-2xl font-bold uppercase tracking-wider dark:text-brand-d">
-    {title}.
-  </h2>
-  {#if desc}
-    <div
-      class=" flex max-w-80 gap-8 border-l-4 border-outline bg-overlay py-2 pl-2 dark:border-outline-d dark:bg-overlay-d"
-    >
-      <p class="font-semibold leading-tight">{desc}</p>
-    </div>
-  {/if}
-</div>
+{#if !dark}
+  <div class="flex flex-col gap-4">
+    <h2 class="text-2xl font-bold uppercase tracking-wider dark:text-brand-d">
+      {title}.
+    </h2>
+    {#if desc}
+      <div
+        class=" flex max-w-80 gap-8 border-l-4 border-outline bg-overlay py-2 pl-2 dark:border-outline-d dark:bg-overlay-d"
+      >
+        <p class="font-semibold leading-tight">{desc}</p>
+      </div>
+    {/if}
+  </div>
+{:else}
+  <div class="flex flex-col gap-4">
+    <h2 class="text-2xl font-bold uppercase tracking-wider text-brand-d">
+      {title}.
+    </h2>
+    {#if desc}
+      <div
+        class=" flex max-w-80 gap-8 border-l-4 border-frame-d bg-overlay-d py-2 pl-2 text-primary-d"
+      >
+        <p class="font-semibold leading-tight">{desc}</p>
+      </div>
+    {/if}
+  </div>
+{/if}
