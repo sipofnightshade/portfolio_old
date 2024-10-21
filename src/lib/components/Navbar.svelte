1a347d6f5ba66ef8fc721c0eb0abaee5fab2fe61
sipofnightshade <gregorymcmillan96@gmail.com>
2024-10-20 22:14:57 -0400

    updated-- dark mode styles for navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,12 +1,13 @@
 <div class="flex w-full justify-between py-5 text-xl">
   <a href="/" class="text-2xl font-bold">Greg.</a>
-  <nav class="flex items-center gap-9 text-lg font-bold capitalize">
+  <nav class="flex items-center gap-9 text-lg font-semibold">
     <a href="/about">About</a>
     <a href="/work">Projects</a>
 
     <a
       href="/mailto:info@gregorymcmillan.me"
-      class="border-2 border-black bg-brand px-4 py-2 leading-none">Let's Talk</a
+      class="border-2 border-black bg-brand px-4 py-2 leading-none dark:border-none dark:bg-brand-dark dark:text-black"
+      >Let's talk!</a
     >
   </nav>
 </div>
