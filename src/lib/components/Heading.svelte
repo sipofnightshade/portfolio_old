0e01bed53ae115bbeb2601deee572cfb154e0b6d
sipofnightshade <gregorymcmillan96@gmail.com>
2024-10-22 08:20:26 -0400

    added-- Heading component for pages

--- src/lib/components/Heading.svelte
+++ src/lib/components/Heading.svelte
@@ -7,8 +7,10 @@
   let { title, desc }: Props = $props();
 </script>
 
-<div class="mt-64">
-  <h1 class="">
+<div
+  class="border--2 container mx-auto mb-16 border-outline bg-none pb-8 pt-64 dark:border-outline-d"
+>
+  <h1 class="text-7xl font-bold">
     {title}
   </h1>
 </div>
