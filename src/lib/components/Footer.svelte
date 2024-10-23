4b0a0db669dff77bf56a944fe8b982d3cca9757d
sipofnightshade <gregorymcmillan96@gmail.com>
2024-10-23 03:30:32 -0400

    update-- added privacy policy outline page

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,6 +1,4 @@
 <footer class="col-span-2 my-8 flex items-center justify-between p-0 font-medium">
   <a href="emailto:info@gregorymcmillan.me" class="text-xl">info@gregorymcmillan.me</a>
-  <a href="https://gregorymcmillan.me/privacy" target="_blank" class="text-xl"
-    >privacy policy</a
-  >
+  <a href="/privacy-policy" target="_blank" class="text-xl">Privacy Policy</a>
 </footer>
---
+++ src/routes/privacy-policy/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import Heading from '$lib/components/Heading.svelte';
+
+  interface Props {}
+
+  let {}: Props = $props();
+</script>
+
+<Heading title="Privacy Policy" />
+
+<div class="container mx-auto">
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, aperiam quo. Ipsum,
+    beatae voluptas repudiandae porro adipisci totam voluptatibus eos vero, a quasi sit
+    aut eveniet modi nulla, sunt fugit molestias impedit necessitatibus repellat. Soluta
+    architecto impedit provident, quaerat deserunt commodi non iste laborum illo
+    exercitationem modi quisquam obcaecati possimus! Nulla ipsa rerum soluta qui id quod
+    mollitia quae, asperiores perspiciatis consectetur praesentium! Sint nisi, aliquam
+    odio odit id corporis suscipit itaque similique, aperiam commodi consequatur neque!
+    Maiores veniam possimus, nobis eligendi voluptate quam explicabo consequuntur totam
+    nam rerum odio blanditiis molestias perferendis numquam quis adipisci animi at
+    molestiae eius assumenda exercitationem enim repellat corrupti! Placeat qui quibusdam
+    error earum et eveniet ratione minima hic voluptates repudiandae similique, nesciunt
+    dicta eum, omnis doloribus consequuntur? Aperiam consectetur laboriosam fuga, dolorum
+    praesentium omnis doloribus et, nam facilis libero assumenda quaerat saepe minima
+    veniam esse totam similique deserunt repellendus quis ea aut nihil quo ullam dolores!
+    In est libero laborum dolorum? Quis, error laborum a officia culpa quam explicabo
+    voluptatem ipsam minima voluptates ex asperiores nihil, ab quo temporibus consequatur
+    excepturi minus, ratione dolores ipsa voluptatum ducimus tenetur? Pariatur earum sequi
+    ratione ipsum nesciunt aliquid dicta amet, perspiciatis ea necessitatibus, saepe
+    minima omnis.
+  </p>
+</div>
