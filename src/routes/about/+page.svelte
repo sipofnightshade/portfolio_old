78c59a89284bfad1cda0938020ed95cd67f1c558
sipofnightshade <gregorymcmillan96@gmail.com>
2024-10-23 03:32:42 -0400

    added-- about Page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Heading from '$lib/components/Heading.svelte';
+  import Contact from '$lib/components/LandingSections/Contact.svelte';
+  import InfiniteScrollingBanner from '$lib/components/LandingSections/InfiniteScrollingBanner.svelte';
+  import Serivces from '$lib/components/LandingSections/Serivces.svelte';
+
+  interface Props {}
+
+  let {}: Props = $props();
+</script>
+
+<Heading title="Who I am." />
+
+<div class="container mx-auto pb-40 pt-16">
+  <p class="text-7xl font-semibold leading-tight tracking-tight">
+    I am a Frontend Developer and UI/UX Designer from the Caribbean—one of the many small
+    islands. I specialize in crafting intuitive interfaces that bridge the gap between
+    technology and everyday life.
+  </p>
+</div>
+<InfiniteScrollingBanner />
+<Serivces />
+<Contact />
