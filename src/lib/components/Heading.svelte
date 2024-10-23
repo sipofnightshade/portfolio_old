fad067f04153bb5f817807397b2b4571ce8d525b
sipofnightshade <gregorymcmillan96@gmail.com>
2024-10-23 03:30:44 -0400

    added-- bottom border to Heading component

--- src/lib/components/Heading.svelte
+++ src/lib/components/Heading.svelte
@@ -7,10 +7,12 @@
   let { title, desc }: Props = $props();
 </script>
 
-<div
-  class="border--2 container mx-auto mb-16 border-outline bg-none pb-8 pt-64 dark:border-outline-d"
->
+<div class="container mx-auto mb-16 flex flex-col gap-8 pb-8 pt-64">
   <h1 class="text-7xl font-bold">
     {title}
   </h1>
+
+  <div class="h-1 w-full bg-outline dark:bg-outline-d">
+    <div class="h-1 w-1/5 bg-brand dark:bg-brand-d"></div>
+  </div>
 </div>
