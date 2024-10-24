5390729b5ee25705e723a4e3ddc392213930cac6
sipofnightshade <gregorymcmillan96@gmail.com>
2024-10-24 11:21:01 -0400

    added-- xs: breakpoint to tailwind

--- src/lib/components/ShowBreakpoint.svelte
+++ src/lib/components/ShowBreakpoint.svelte
@@ -1,10 +1,11 @@
 <div
   class="fixed right-3 top-3 z-50 rounded bg-purple-500 px-3 py-2 font-bold uppercase text-primary"
 >
-  <span class="sm:hidden">base</span>
+  <span class="xs:hidden">base</span>
+  <span class="xs:block hidden sm:hidden">xs</span>
   <span class="hidden sm:block md:hidden">sm</span>
   <span class="hidden md:block lg:hidden">md</span>
   <span class="hidden lg:block xl:hidden">lg</span>
   <span class="hidden xl:block 2xl:hidden">xl</span>
-  <span class="3xl:hidden hidden 2xl:block">xl</span>
+  <span class="3xl:hidden hidden 2xl:block">2xl</span>
 </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,18 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   darkMode: 'selector',
   theme: {
+    container: {
+      padding: '0.5rem',
+      center: true
+    },
+    screens: {
+      xs: '460px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px'
+    },
     fontFamily: {
       sans: ['Onest Variable', 'sans-serif']
     },
