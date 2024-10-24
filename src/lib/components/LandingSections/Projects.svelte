ad8ec1aaa79e22899a60cb6e4beae87cd4297dde
sipofnightshade <gregorymcmillan96@gmail.com>
2024-10-24 18:59:06 -0400

    updated-- projects section to be responsive

--- src/lib/components/LandingSections/Projects.svelte
+++ src/lib/components/LandingSections/Projects.svelte
@@ -50,6 +50,7 @@
       panels.forEach((panel, index) => {
         gsap.to(panel, {
           y: 0, // Translate Y to 0
+          opacity: 1, // Show the panel
           duration: 2,
           ease: 'power2.out',
           scrollTrigger: {
@@ -78,37 +79,40 @@
 </script>
 
 {#snippet Panel(data: string)}
-  <a href="/" class="panel group my-12 flex translate-y-44 flex-col gap-3">
+  <a
+    href="/"
+    class="panel group my-2 flex flex-col gap-3 sm:my-4 md:my-12 md:translate-y-44 md:opacity-30"
+  >
     <!-- image here -->
     <div class="aspect-[4/3] bg-overlay dark:bg-overlay-d"></div>
 
-    <div class="flex items-end justify-between pl-2">
-      <h3 class="text-2xl font-bold">{data}</h3>
+    <div class="flex items-end justify-between md:pl-2">
+      <h3 class="text-xl font-bold sm:text-2xl">{data}</h3>
 
       <div
-        class="flex h-14 w-14 items-center justify-center bg-zinc-300 duration-300 group-hover:bg-brand"
+        class="hidden h-11 w-11 items-center justify-center bg-zinc-300 duration-300 group-hover:bg-brand md:flex lg:h-14 lg:w-14"
       >
         <Link
-          class="h-5 w-5 rotate-45 text-black transition-transform duration-300 group-hover:rotate-0"
+          class="h-4 w-4 rotate-45 text-black transition-transform duration-300 group-hover:rotate-0 lg:h-5 lg:w-5"
         />
       </div>
     </div>
   </a>
 {/snippet}
 
-<section id="work" class="relative {className}">
-  <div id="progressBar" class="absolute left-0 top-0 h-screen w-2">
+<section id="work" class="relative mt-28 md:mt-0 {className}">
+  <div id="progressBar" class="absolute left-0 top-0 hidden h-screen w-2 md:block">
     <div bind:this={progressBar} class="w-full bg-brand dark:bg-brand-d"></div>
   </div>
 
-  <!-- left side -->
-  <div class="container mx-auto grid h-full grid-cols-2 gap-2">
-    <div id="ftLeft" class="flex h-screen flex-col justify-between pb-20 pt-40">
+  <div class="container h-full gap-2 md:grid md:grid-cols-2">
+    <!-- left side -->
+    <div id="ftLeft" class="flex flex-col justify-between md:h-screen md:pb-20 md:pt-40">
       <SectionHeading
         title="projects"
         desc="A compilation of some of my favourite projects."
       />
-      <div class="flex items-start gap-2 font-bold leading-none">
+      <div class="hidden items-start gap-2 font-bold leading-none md:flex">
         <p class="text-9xl text-primary dark:text-primary-d">{currentPanel}</p>
         <div class="pt-2.5 text-4xl text-muted dark:text-muted-d">
           /{totalPanels}
@@ -118,7 +122,7 @@
     <!-- right side -->
     <div
       id="ftRight"
-      class="relative flex h-full flex-col gap-10 border-x border-outline pb-16 pt-24 dark:border-outline-d"
+      class="relative flex h-full flex-col gap-4 border-outline pb-10 pt-8 md:gap-10 md:border-x md:pt-24 lg:pb-16 dark:border-outline-d"
     >
       <!-- <CornerEdges class="h-2" strokeWidth={20} /> -->
 
