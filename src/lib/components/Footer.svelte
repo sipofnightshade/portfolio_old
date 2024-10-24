ab9413b8e011abedb2db92fc0b2c602348be48fa
sipofnightshade <gregorymcmillan96@gmail.com>
2024-10-24 09:25:26 -0400

    updated-- footer font size

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,4 @@
 <footer class="col-span-2 my-8 flex items-center justify-between p-0 font-medium">
-  <a href="emailto:info@gregorymcmillan.me" class="text-xl">info@gregorymcmillan.me</a>
-  <a href="/privacy-policy" target="_blank" class="text-xl">Privacy Policy</a>
+  <a href="emailto:info@gregorymcmillan.me" class="text-sm">info@gregorymcmillan.me</a>
+  <a href="/privacy-policy" target="_blank" class="text-sm">Privacy Policy</a>
 </footer>
