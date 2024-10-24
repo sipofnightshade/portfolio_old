3d331bba747b79de0c54fe5f193cfa1d80db1e82
sipofnightshade <gregorymcmillan96@gmail.com>
2024-10-24 18:32:41 -0400

    updated-- section headings and about section sizes and leading

--- src/lib/components/LandingSections/About.svelte
+++ src/lib/components/LandingSections/About.svelte
@@ -8,11 +8,11 @@
 </script>
 
 <section class="relative mt-20 bg-overlay dark:bg-overlay-d {className}">
-  <div class="container mx-auto flex flex-col gap-8 py-28 md:py-40">
+  <div class="container mx-auto flex flex-col gap-4 py-28 md:py-40 lg:gap-8">
     <SectionHeading title="about" />
 
     <p
-      class="xs:text-3xl text-2xl font-semibold leading-tight tracking-tight sm:text-5xl md:text-6xl 2xl:text-7xl"
+      class="xs:text-3xl text-2xl font-semibold !leading-tight tracking-tight sm:text-4xl md:text-5xl lg:text-6xl 2xl:text-7xl"
     >
       I am a Frontend Developer and UI/UX Designer from the Caribbean—one of the many
       small islands. I specialize in crafting intuitive interfaces that bridge the gap
--- src/lib/components/SectionHeading.svelte
+++ src/lib/components/SectionHeading.svelte
@@ -3,28 +3,28 @@
 </script>
 
 {#if !dark}
-  <div class="flex flex-col gap-4">
-    <h2 class="text-2xl font-bold uppercase tracking-wider dark:text-brand-d">
+  <div class="flex flex-col gap-3 lg:gap-4">
+    <h2 class="text-lg font-bold uppercase tracking-wider lg:text-2xl dark:text-brand-d">
       {title}.
     </h2>
     {#if desc}
       <div
-        class=" flex max-w-80 gap-8 border-l-4 border-outline bg-overlay py-2 pl-2 dark:border-outline-d dark:bg-overlay-d"
+        class="flex max-w-80 gap-8 border-l-4 border-outline bg-overlay py-2 pl-2 dark:border-outline-d dark:bg-overlay-d"
       >
-        <p class="font-semibold leading-tight">{desc}</p>
+        <p class="text-sm font-semibold leading-tight lg:text-base">{desc}</p>
       </div>
     {/if}
   </div>
 {:else}
-  <div class="flex flex-col gap-4">
-    <h2 class="text-2xl font-bold uppercase tracking-wider text-brand-d">
+  <div class="flex flex-col gap-3 lg:gap-4">
+    <h2 class="text-lg font-bold uppercase tracking-wider text-brand-d lg:text-2xl">
       {title}.
     </h2>
     {#if desc}
       <div
-        class=" flex max-w-80 gap-8 border-l-4 border-frame-d bg-overlay-d py-2 pl-2 text-primary-d"
+        class="flex max-w-80 gap-8 border-l-4 border-frame-d bg-overlay-d py-2 pl-2 text-primary-d"
       >
-        <p class="font-semibold leading-tight">{desc}</p>
+        <p class="text-sm font-semibold leading-tight lg:text-base">{desc}</p>
       </div>
     {/if}
   </div>
