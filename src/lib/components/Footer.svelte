3cd8202189680573786870b6ee9421fbc67eea0a
sipofnightshade <gregorymcmillan96@gmail.com>
2024-10-25 08:46:12 -0400

    updated-- footer text and responsive padding

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,4 @@
-<footer class="col-span-2 my-8 flex items-center justify-between p-0 font-medium">
+<footer class="col-span-2 my-4 flex items-center justify-between p-0 font-medium lg:my-8">
   <a href="emailto:info@gregorymcmillan.me" class="text-sm">info@gregorymcmillan.me</a>
-  <a href="/privacy-policy" target="_blank" class="text-sm">Privacy Policy</a>
+  <a href="/privacy-policy" target="_blank" class="text-sm">Privacy</a>
 </footer>
