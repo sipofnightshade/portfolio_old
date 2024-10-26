8cc78bbebf372f54737868e110dc537aafd8330d
sipofnightshade <gregorymcmillan96@gmail.com>
2024-10-25 22:14:52 -0400

    updated-- footer to be more reusable

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,10 @@
-<footer class="col-span-2 my-4 flex items-center justify-between p-0 font-medium lg:my-8">
-  <a href="emailto:info@gregorymcmillan.me" class="text-sm">info@gregorymcmillan.me</a>
-  <a href="/privacy-policy" target="_blank" class="text-sm">Privacy</a>
+<script lang="ts">
+  let { class: className = '' }: { class?: string } = $props();
+</script>
+
+<footer
+  class="col-span-2 my-4 flex items-center justify-between p-0 font-medium lg:my-8 {className}"
+>
+  <a href="emailto:info@gregorymcmillan.me">info@gregorymcmillan.me</a>
+  <a href="/privacy-policy" target="_blank">Privacy</a>
 </footer>
